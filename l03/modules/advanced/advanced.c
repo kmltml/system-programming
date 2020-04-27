5b7923ebeb846a4adc43dfa2998aaadec44859db
kmltml <kamil.tomala@gmail.com>
2020-04-27 10:49:12 +0200

    Lab03 - Task 2 add the jiffies device

--- l03/modules/advanced/advanced.c
+++ l03/modules/advanced/advanced.c
@@ -9,10 +9,15 @@
 #include <linux/sched/task.h>
 #include <linux/pid.h>
 
+#include <linux/jiffies.h>
+
 MODULE_LICENSE("GPL");
 
 const struct file_operations prname_fops;
 struct miscdevice prname_dev;
+
+const struct file_operations jiffies_fops;
+struct miscdevice jiffies_dev;
 
 char prname_name[TASK_COMM_LEN] = {0};
 
@@ -24,11 +29,17 @@ static int __init circular_init(void)
            "Cannot register the /dev/prname device\n");
     goto err;
   }
+  if ((result = misc_register(&jiffies_dev)) != 0) {
+    printk(KERN_WARNING
+           "Cannot register the /dev/jiffies device\n");
+    goto err;
+  }
   printk(KERN_INFO "Module advanced initialized");
   return 0;
 
  err:
   misc_deregister(&prname_dev);
+  misc_deregister(&jiffies_dev);
   return result;
 }
 
@@ -113,4 +124,39 @@ struct miscdevice prname_dev = {
         .fops = &prname_fops,
         .nodename = "prname",
         .mode = 0666,
+};
+
+ssize_t jiffies_read(struct file *filp, char __user *user_buf, size_t count,
+                     loff_t *f_pos) {
+  char buffer[16];
+
+  uint64_t jiffies = get_jiffies_64();
+
+  size_t length = snprintf(buffer, ARRAY_SIZE(buffer), "%llu", jiffies);
+
+  if (*f_pos >= length) {
+    return 0;
+  }
+
+  size_t bytes_to_copy = min(count, (size_t) (length - *f_pos));
+
+  int result = copy_to_user(user_buf, buffer + *f_pos, bytes_to_copy);
+  if (result != 0) {
+    return result;
+  }
+
+  *f_pos += bytes_to_copy;
+  return bytes_to_copy;
+}
+
+const struct file_operations jiffies_fops = {
+        .read = jiffies_read,
+};
+
+struct miscdevice jiffies_dev = {
+        .minor = MISC_DYNAMIC_MINOR,
+        .name = "jiffies",
+        .fops = &jiffies_fops,
+        .nodename = "jiffies",
+        .mode = 0444,
 };
